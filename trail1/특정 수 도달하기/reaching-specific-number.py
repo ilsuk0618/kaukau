2dba400d04bf8cc3db00d8f066104a5758ce10a8
ilsuk0618 <wschang04@gmail.com>
2026-05-24 15:56:39 +0900

    [Passed] 특정 수 도달하기 (50ms, 16MB)

--- trail1/특정 수 도달하기/reaching-specific-number.py
+++ trail1/특정 수 도달하기/reaching-specific-number.py
@@ -11,10 +11,9 @@ for elem in arr:
 for ele in arr2:
     ans+=ele
 
-if len(arr2)==0:
-    avg="-nan"
-else:
-    avg=ans/len(arr2)
+
+avg=ans/len(arr2)
 
 print(f"{ans} {avg:.1f}")
+##소수 첫째 자리 까지 출력하려면 f-string 사용한다. 
 
