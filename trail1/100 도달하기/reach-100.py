9d1f3fafd6e8dc4bff180f407c02d45439c721af
ilsuk0618 <wschang04@gmail.com>
2026-05-26 11:19:22 +0900

    [Passed] 100 도달하기 (53ms, 16MB)

--- trail1/100 도달하기/reach-100.py
+++ trail1/100 도달하기/reach-100.py
@@ -1,17 +1,13 @@
 i=int(input())
-p=1
-p2=i
 p3=2
 arr=[]
 
-arr.append(p)
-arr.append(p2)
-arr.append(0)
+arr.append(1)
+arr.append(i)
 
 while arr[p3-1]<=100:
+    arr.append(0)
     arr[p3]=arr[p3-1]+arr[p3-2]
-    k=arr[p3]
-    arr.append(k)
+    
     p3+=1
-arr.pop()
 print(*arr)
