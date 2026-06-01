5486602a62df3c66fe5bb84976ff4d4d0fee06c3
ilsuk0618 <wschang04@gmail.com>
2026-06-02 01:15:10 +0900

    [Passed] 격자로 사각형 만들기 (50ms, 16MB)

---
+++ trail1/격자로 사각형 만들기/print-grid-in-rectangle.py
@@ -0,0 +1,15 @@
+n=int(input())
+arr=[[0 for _ in range(n)]
+for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0: 
+            arr[i][j]=1
+        else:
+            arr[i][j]=arr[i-1][j]+arr[i][j-1]+arr[i-1][j-1]
+
+for elem in arr:
+    for ans in elem:
+        print(ans, end=" ")
+    print()
