9594b87f039440eaba45b7060b252f67f2095249
ilsuk0618 <wschang04@gmail.com>
2026-06-02 13:27:08 +0900

    [Passed] 공백을 기준으로 출력 (49ms, 16MB)

--- trail1/공백을 기준으로 출력/output-based-on-space.py
+++ trail1/공백을 기준으로 출력/output-based-on-space.py
@@ -4,10 +4,7 @@ arr=[]
 
 for elem in i:
     if elem!= " ":
-        arr.append(elem)
+        print(elem, end="")
 for elem in j:
     if elem!=" ":
-        arr.append(elem)
-
-for elem in arr:
-    print(elem, end="")
+        print(elem, end="")
