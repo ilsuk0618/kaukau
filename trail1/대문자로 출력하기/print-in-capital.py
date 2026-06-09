9351ac749687454dfbc79e4f2f3bcd7f2d59a9a0
ilsuk0618 <wschang04@gmail.com>
2026-06-09 22:37:45 +0900

    [Passed] 대문자로 출력하기 (48ms, 16MB)

--- trail1/대문자로 출력하기/print-in-capital.py
+++ trail1/대문자로 출력하기/print-in-capital.py
@@ -1,9 +1,5 @@
 arr=input()
-arr2=[]
 
 for elem in arr:
     if 64<ord(elem)<91 or 96<ord(elem)<123:
-        arr2.append(elem.upper())
-
-for elem in arr2:
-    print(elem, end="")
+        print(elem.upper(), end="")
