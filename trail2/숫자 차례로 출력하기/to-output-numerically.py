4c51c9bc1a56340173648e0853948c767cf37377
ilsuk0618 <wschang04@gmail.com>
2026-06-25 23:54:33 +0900

    [Passed] 숫자 차례로 출력하기 (51ms, 16MB)

--- trail2/숫자 차례로 출력하기/to-output-numerically.py
+++ trail2/숫자 차례로 출력하기/to-output-numerically.py
@@ -1,12 +1,12 @@
 n = int(input())
 
-def type(a):
-    if a==n+1:
+def type(n):
+    if n==0:
         return
-    
-    print(a, end=" ")
-    type(a+1)
-        
+
+    type(n-1)
+    print(n, end=" ")
+     ##재귀함수 속 프린트 위치의 중요성...굳이 처음에 1넣고 하나씩 늘리지 않아도 프린트위치만 조절해도 n 바로 넣어도 됨.
 def type2(n):
     if n==0:
         return
@@ -14,6 +14,6 @@ def type2(n):
     print(n, end=" ")
     type2(n-1)
 
-type(1)
+type(n)
 print()
 type2(n)
