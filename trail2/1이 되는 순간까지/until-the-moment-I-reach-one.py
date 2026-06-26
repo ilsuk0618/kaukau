a6e3ab2f37570aad53494ac517901c8d99af0ec1
ilsuk0618 <wschang04@gmail.com>
2026-06-26 15:13:27 +0900

    [Passed] 1이 되는 순간까지 (51ms, 16MB)

--- trail2/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ trail2/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -2,7 +2,7 @@ N = int(input())
 
 def share(N):
     if N==1:
-        return 1
+        return 0
 
     elif N%2==0:
         return share(N//2) + 1
@@ -10,4 +10,4 @@ def share(N):
     else:
         return share(N//3) +1
 
-print(share(N)-1)
+print(share(N))
